08f83b3216c042816fdaabb2a3b5f98006a48816
anaoktaa <ana.oktaviana.ana@gmail.com>
2020-09-03 11:02:32 +0700

    adding mock

--- unit-test/script2.test.js
+++ unit-test/script2.test.js
@@ -16,3 +16,21 @@ it('calls swapi to get people with promise', () => {
 
     });
 });
+
+it('getPeople return count and results', () => {
+    const mockFetch = jest.fn()
+        .mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                count: 82,
+                results: [0,1,2,3,4,5]
+            })
+        }));
+
+    expect.assertions(4);
+    return swapi.getPeoplePromise(mockFetch).then(data => {
+        expect(mockFetch.mock.calls.length).toBe(1);
+        expect(mockFetch).toBeCalledWith('https://swapi.dev/api/people');
+        expect(data.count).toEqual(82);
+        expect(data.results.length).toBeGreaterThan(5);
+    });
+})
